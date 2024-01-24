16b1e359ea5c03a22f045e28a6497373eda42485
RaduRalucag <ralucar2004@gmail.com>
2024-01-24 19:09:45 +0200

    Add project files.

---
+++ ExamenDAW/Helpers/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+namespace ExamenDAW.Helpers.Extensions
+{
+    public static class ServiceExtension{ 
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            //services.AddTransient<IRepository, Repository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            //services.AddTransient<IService,Service>();
+            return services;
+        }
+    }
+}
